0ba7772efb88f4e41fece5a296ae4db45d7afe30
GUTT-JuanVazquez <71057053+GUTT-JuanVazquez@users.noreply.github.com>
2022-12-07 09:42:20 -0600

    [ADD] module book detail, enviroment

--- src/components/User/BookDetail.vue
+++ src/components/User/BookDetail.vue
@@ -8,7 +8,7 @@
         </div><br>
         <div class="row text-start">
             <div class="col-lg-3 ">
-                <img v-bind:src="`http://127.0.0.1:8000/api/image/${libro.imagen}`" class="w-100" >
+                <img v-bind:src="`${urlapi}image/${libro.imagen}`" class="w-100" >
             </div>
             <div class="col-lg-9">
                 <div class="row">
@@ -46,7 +46,7 @@
 
         <div class="row">
             <div class="col-lg-3 d-grid gap-2 p-2">
-                <a v-bind:href="'http://127.0.0.1:8000/api/file/'+libro.pdf" type="button" class="btn btn-primary" download>Descargar PDF</a>
+                <a v-bind:href="urlapi+'file/'+libro.pdf" type="button" class="btn btn-primary" download>Descargar PDF</a>
             </div>
 
             <div class="col-lg-3 d-grid gap-2 p-2">
@@ -57,22 +57,46 @@
 
         <br><br><br>
 
+        
+
         <!-- Recomendaiones -->
         <div class="row text-start">
             <h2>Similares</h2>
         </div>
+
+    <!-- spinner -->
+    <div v-show="isLoading" class="spinner-border text-dark text-center" style="width: 3rem; height: 3rem;" role="status">
+    </div>
+
+
         <div class="row">
-            <!-- Carrusel de libros similares -->
-            <br>
+                <!-- Libros guardados -->
+            <div v-show="!isLoading" style="width:100%;">
+                
+                <div
+                v-show="!isLoading"
+                class="bloque"
+                v-if="libros.length > 0"
+                style="width: 100%; display: inline-block; margin: 1rem;"
+                >
+                <div class="m-2 bloque" v-for="(cat, index) in libros" :key="cat">
+                    <div class="card-book" style="float: left">
+                    <img v-bind:src="`${urlapi}image/${cat.imagen}`" class="img-card" @click="showBook(cat.id)"/>
+                    <div class="row align-items-start mt-2">
+                        <div class="col text-start">
+                            <b class="letter-page book col-md-9 text-white" >{{ cat.titulo }}</b>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+                </div>
+                <div v-show="!isLoading" v-else class="text-center pb-5 pt-5">
+                    <h3 class="title-main m-4">No tienes hay libros similares</h3>
+                </div>
+            </div>
+        <br>
         </div>
 
-        <div class="row text-start">
-            <span style="color: grey;">Un amor para sentir y desear</span>
-            <h2>Romance erotico</h2>
-        </div>
-        <div class="row text-center">
-            <!-- Carrusel e libros recomedados -->
-        </div>
 
     </div>
 </template>
@@ -85,12 +109,17 @@ import axios from 'axios';
 export default {
     data(){
         return {
+            isLoading:false,
             libro:{},
-            user:{}
+            user:{},
+            libros:[],
+            urlapi:url
         }
     },
-    mounted(){
-        this.getLibro(this.$route.params.id)
+    async mounted(){
+        
+        await this.getLibro(this.$route.params.id)
+        this.getBooksByCategory()
     },
     methods:{
         getToken(){
@@ -164,7 +193,38 @@ export default {
                     )
             })
         },
+        getBooksByCategory() {
+        this.isLoading = true
+        axios.get(url+"categories/"+this.libro.category_id+"/books",{
+            headers:{
+                'Authorization': `Bearer ${this.getToken()}` 
+            }
+            })
+            .then((response) => {
+
+            this.libros = response.data.message;
+            for (let index = 0; index < this.libros.length; index++) {
+                if(this.libros[index]['id'] == this.$route.params.id){
+                    this.libros.splice(index,1);
+                }
+            }
+            shuffleArray(this.libros);
+            console.log(response.data.message);
+            console.log(this.libros);
+            })
+            .catch((error) => console.log(error),this.isLoading = false);
+        },
+        async showBook(id) {
+            
+            await this.$router.push('/book/'+id);
+            this.$router.go(this.$router.currentRoute)
+            //window.location.href = "http://localhost:5173/book/"+id;
+        },
     }
+}
+
+function shuffleArray(inputArray){
+    inputArray.sort(()=> Math.random() - 0.5);
 }
 </script>
 <style>
