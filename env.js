14572d4fbdc9526493714c6e4ac928252b964e57
AngelZapata582 <angelzapata582@gmail.com>
2022-12-08 15:02:04 -0600

    [FIX] endpoint;

--- env.js
+++ env.js
@@ -1,6 +1,6 @@
 const env = {
-    //API_URL: 'https://larave-h-books-api-production.up.railway.app/api/',
-    API_URL:'http://localhost:8000/api/'
+    API_URL: 'https://larave-h-books-api-production.up.railway.app/api/',
+    //API_URL:'http://localhost:8000/api/'
 };   
 
 export default env;
