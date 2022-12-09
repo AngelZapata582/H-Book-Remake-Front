3e7342741535aec35126818c87c7fba7962b8a4e
AngelZapata582 <angelzapata582@gmail.com>
2022-12-08 20:31:27 -0600

    [UPDATE] change url;

--- env.js
+++ env.js
@@ -1,6 +1,7 @@
 const env = {
-    API_URL: 'https://larave-h-books-api-production.up.railway.app/api/',
-    //API_URL:'http://localhost:8000/api/'
+    //API_URL: 'https://larave-h-books-api-production.up.railway.app/api/',
+    //API_URL:'http://localhost:8000/api/',
+    API_URL:'https://523495a0189f.ngrok.io/api/'
 };   
 
 export default env;
--- enviroment.js
+++ enviroment.js
@@ -1,2 +1,3 @@
-export default URL = 'https://larave-h-books-api-production.up.railway.app/api/'
+//export default URL = 'https://larave-h-books-api-production.up.railway.app/api/'
 //export default URL = 'http://localhost:8000/api/'
+export default URL = 'https://523495a0189f.ngrok.io/api/'
